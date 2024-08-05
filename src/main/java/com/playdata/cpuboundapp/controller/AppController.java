0bfd52a00e7823cc6b46f0e3d4de668b294a0a10
dev-library <96283221+dev-library@users.noreply.github.com>
2024-08-05 13:33:50 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -33,4 +33,11 @@ public class AppController {
 
         return myHash;
     }
+
+    @GetMapping("/app-health")
+    public String healthCheck() {
+        return "app_health_status 1.0\n# EOF";
+    }
+
+    
 }
